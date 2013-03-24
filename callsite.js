0257cd758a19b8079ee4f8870fbd3a9717661196
Matt Robenolt <matt@ydekproductions.com>
2013-03-24 16:27:09 -0700

    FF is sorta compatible with the API now

--- callsite.js
+++ callsite.js
@@ -1,7 +1,9 @@
 ;(function(window, Error) {
 
-  function CallSite(line) {
+  function CallSite(struct) {
     var self = this;
+
+    this.func = struct['function'];
 
     this.getThis = function getThis() {
       return self;
@@ -12,27 +14,27 @@
     };
 
     this.getFunction = function getFunction() {
-      //
+      return this.func;
     };
 
     this.getFunctionName = function getFunctionName() {
-      //
+      return struct.functionName;
     };
 
     this.getMethodName = function getMethodName() {
-      //
+      return struct.methodName;
     };
 
     this.getFileName = function getFileName() {
-      //
+      return struct.fileName;
     };
 
     this.getLineNumber = function getLineNumber() {
-      //
+      return struct.lineNumber || 0;
     };
 
     this.getColumnNumber = function getColumnNumber() {
-      //
+      return struct.columnNumber || 0;
     };
 
     this.getEvalOrigin = function getEvalOrigin() {
@@ -54,21 +56,98 @@
     this.isConstructor = function isConstructor() {
       //
     };
+
+    this.getArguments = function getArguments() {
+      // lol, I'm going to add this
+    };
+
+    this.toString = function toString() {
+      var location = [struct.fileName, struct.lineNumber, struct.columnNumber].join(':');
+      return struct.functionName + ' (' + location + ')';
+    };
   }
+
+  function callSiteFactory(error) {
+    var factory,
+        stack = error.stack,
+        name = error.name,
+        message = error.message;
+
+    if (error.stacktrace) {
+      factory = makeOperaCallSite;
+      stack = error.stacktrace;
+    }
+
+    return {
+      makeStruct: factory || makeFireFoxCallSite,
+      name: name,
+      message: message,
+      stack: stack
+    };
+  }
+
+  var FireFox_re = /^([^@]*)@(.*?):(\d+)(?::(\d+))?$/;
+
+  function makeFireFoxCallSite(line, func) {
+    var match = line.match(FireFox_re);
+    return {
+      functionName: match[1] || '<anonymous>',
+      fileName: match[2] || '',
+      lineNumber: ~~match[3] || 0,
+      columnNumber: ~~match[4] || 0,
+      'function': func
+    };
+  }
+
+  function makeOperaCallSite(line) {
+    // derp
+  }
+
+  var function_re = /function\s*(.*?)\((.*?)\)/;
 
   Error.captureStackTrace = function captureStackTrace(error, topLevel) {
     // Simultaneously traverse the frames in error.stack and the arguments.caller
     // to build a list of CallSite objects
-    var nativeStack = error.stack;
+    var factory = callSiteFactory(error);
+    var nativeStack = factory.stack.split('\n');
+    var c = arguments.callee;
     var frames = [];
+    while (c = c.caller) {
+      frames.push(new CallSite(factory.makeStruct(nativeStack.shift(), c)));
+    }
+
+    // Explicitly set back the error.name and error.message
+    error.name = factory.name;
+    error.message = factory.message;
+
+    var error_string = error.name + ': ' + error.message;
 
     // Pass the raw callsite objects through and get back a formatted stack trace
-    error.stack = Error.prepareStackTrace(error, frames);
+    var makeStack = Error.prepareStackTrace || defaultPrepareStackTrace;
+    error.stack = makeStack(error_string, frames);
   };
 
-  Error.prepareStackTrace = function prepareStackTrace(error, frames) {
-    // Do magic to return the default stack trace that mimics V8 as much as possible
-    return frames;
-  };
+  function defaultPrepareStackTrace(error_string, frames) {
+    // Adapted from V8 source:
+    // https://github.com/v8/v8/blob/1613b7/src/messages.js#L1051-L1070
+    var lines = [];
+    lines.push(error_string);
+    for (var i = 0; i < frames.length; i++) {
+      var frame = frames[i];
+      var line;
+      try {
+        line = frame.toString();
+      } catch (e) {
+        try {
+          line = "<error: " + e + ">";
+        } catch (ee) {
+          // Any code that reaches this point is seriously nasty!
+          line = "<error>";
+        }
+      }
+      lines.push("    at " + line);
+    }
+    return lines.join('\n');
+  }
 
 })(this, Error);
